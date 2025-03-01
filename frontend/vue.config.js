b29c3e49d6dcf00d12c4312019da419b6c369687
Ongun <info@ongunakay.com>
2025-03-02 02:59:49 +0300

    vue.config.js updated.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,35 +1,20 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-  
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-
   configureWebpack: {
     output: {
       filename: '[name].[contenthash].js',
     },
   },
-
   productionSourceMap: false,
-
   css: {
     extract: true,
     sourceMap: false,
   },
-
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'Duolingo Clone';
       return args;
     });
   },
-
-  outputDir: 'public',
+  outputDir: 'frontend/dist',
 };
