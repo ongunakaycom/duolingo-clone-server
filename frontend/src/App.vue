c8f8e1432cf714babfa7cb6db75ae4b4382160ad
Ongun <info@ongunakay.com>
2025-03-02 15:22:24 +0300

    vue components updated.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <AppHeader />
+    <router-view />
+    <AppFooter />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+import router from './router'; // Import Vue Router
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AppHeader,
+    AppFooter
+  },
+  router // Use Vue Router
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <LandingJumbotron />
+    <LandingFeatures />
+  </div>
+</template>
+
+<script>
+import LandingJumbotron from '@/components/LandingJumbotron.vue';
+import LandingFeatures from '@/components/LandingFeatures.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    LandingJumbotron,
+    LandingFeatures
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
