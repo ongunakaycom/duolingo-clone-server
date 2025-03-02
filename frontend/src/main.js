a29d0b11a7046ebfaff3e544cf7570f2ebf76c7b
Ongun <info@ongunakay.com>
2025-03-02 21:24:43 +0300

    router updated on main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from '../src/router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router); // Register the router
+app.mount('#app');
